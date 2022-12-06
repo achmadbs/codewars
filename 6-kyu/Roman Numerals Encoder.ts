cfd26b64336df8e3747db74500e454f5f5116e33
achmad <achmadbdrdn@gmail.com>
2022-12-06 10:18:45 +0700

    Roman Numerals Encoder

---
+++ 6-kyu/Roman Numerals Encoder.ts
@@ -0,0 +1,25 @@
+export function solution(number: number): string {
+  const romanNumerals: { [key: string]: any } = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let result = '';
+  for (let roman in romanNumerals) {
+    while (number >= romanNumerals[roman]) {
+      result += roman;
+      number -= romanNumerals[roman];
+    }
+  }
+  return result;
+}
