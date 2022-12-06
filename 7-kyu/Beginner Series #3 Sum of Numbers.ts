d4607f0c42859289d2bcb0bd0564bd41b8a48631
achmad <achmadbdrdn@gmail.com>
2022-12-06 09:42:45 +0700

    Beginner Series #3 Sum of Numbers

---
+++ 7-kyu/Beginner Series #3 Sum of Numbers.ts
@@ -0,0 +1,13 @@
+export function getSum(a: number, b: number): number {
+  if (a === b) return a;
+
+  let min = Math.min(a, b);
+  let max = Math.max(a, b);
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
